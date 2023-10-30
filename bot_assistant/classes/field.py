d61ec5caa7317d779969d470c3df04830fb6df7e
Mykyta Olenykov <nikita2001395@gmail.com>
2023-10-30 22:51:39 +0200

    add: class field get and set

--- bot_assistant/classes/field.py
+++ bot_assistant/classes/field.py
@@ -1,6 +1,14 @@
 class Field:
     def __init__(self, value):
-        self.value = value
+        self._value = value
 
     def __str__(self):
         return str(self.value)
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, new_value):
+        self._value = new_value
