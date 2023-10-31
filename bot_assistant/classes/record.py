cf46751cf53b4d7fe72522c86a21abd1e4a76b19
Skygut <v.chub@outlook.de>
2023-10-31 20:41:35 +0100

    add class email

--- bot_assistant/classes/record.py
+++ bot_assistant/classes/record.py
@@ -1,2 +1,14 @@
+from bot_assistant.classes import Email
+
+
 class Record:
-    pass
+    def __init__(self, name):
+        self.email = None
+
+    def add_email(self, email_data: str):
+        """Adds an email address"""
+        self.email = Email(email_data)
+
+    def del_email(self):
+        """Deletes an email address"""
+        self.email = None
--- bot_assistant/controllers/emails_ctrl.py
+++ bot_assistant/controllers/emails_ctrl.py
@@ -1,2 +1,28 @@
 class EmailsCtrl:
     pass
+
+
+def add_email_func(book):
+    name = input("Please enter name: ")
+    name = name.strip().title()
+    if name not in book.data:
+        return book, "\n<<< This user not in contact book.\n"
+    contact = book[name]
+    if contact.email:
+        return book, "\n<<< Email for this contact already exist\n"
+    email = input("Please enter email address: ")
+    email = email.strip()
+    contact.add_email(email)
+    return book, f"\n<<< Email: [{email}] has been added to contact: [{name}]\n"
+
+
+def change_email_func(book):
+    name = input("Please enter name: ")
+    name = name.strip().title()
+    if name not in book.data:
+        return book, "\n<<< This user not in contact book.\n"
+    contact = book[name]
+    email = input("Please enter new email: ")
+    email = email.strip()
+    contact.add_email(email)
+    return book, f"\n<<< Email: [{email}] has been changed to contact [{name}]\n"
